93704972e8381dd2963f4906b6e82d21c32c77a4
kilowat <kadantsev.sergey@gmail.com>
2016-10-21 15:16:07 +0300

    init

--- src/js/main.js
+++ src/js/main.js
@@ -7,21 +7,27 @@ $(document).ready(function () {
 
   //run sliderprice
 
- $('.slider-gaps').limitslider({
-    values: [1, 400],
+  $('.slider-gaps').limitslider({
+    values: [parseInt($('#min-price').val()), parseInt($('#max-price').val())],
     gap: 1,
     step: 1,
-    min:1,
-    max:400,
+    min: parseInt($('#min-price').val()),
+    max: parseInt($('#max-price').val()),
     label: true,
     showRanges: true,
-     slide: function(event, ui) {
-      console.log(ui.values[0]);
-      console.log(ui.values[1]);
+    slide: function (event, ui) {
+      $('#min-price').next().val(ui.values[0]);
+      $('#max-price').next().val(ui.values[1]);
     },
     ranges: [false, {
       styleClass: 'range-glow'
     }, false, true]
   });
-  
+
+  $('#min-price').next().change(function () {
+    $(".slider-gaps").limitslider('values', 0, $(this).val());
+  });
+  $('#max-price').next().change(function () {
+    $(".slider-gaps").limitslider('values', 1, $(this).val());
+  });
 });
