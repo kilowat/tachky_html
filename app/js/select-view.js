5369b020c34e56d03403e02f2e479826be845483
Kilowat <kadantsev.sergey@gmail.com>
2016-10-23 15:28:22 +0300

    init

---
+++ app/js/select-view.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  var templateNameKey = "tamplate_view";
+  var tableView = "view-table";
+  var listView = "view-list";
+  var miniView = "mini";
+  var viewId = '#catalog-view';
+  var className = Cookies.get(templateNameKey) || tableView;
+  
+  var setView = function(className) {
+    Cookies.set(templateNameKey,className);
+
+    switch (className) {
+    case tableView:
+      $(viewId).removeClass(listView);
+      $(viewId).removeClass(miniView);
+      $(viewId).addClass(tableView);
+      break;
+    case listView:
+      $(viewId).removeClass(tableView);
+      $(viewId).removeClass(miniView);
+      $(viewId).addClass(listView);
+      break;
+    case miniView:
+      $(viewId).removeClass(tableView);
+      $(viewId).addClass(miniView);
+      $(viewId).addClass(listView);
+    }
+  }
+  
+  setView(className);
+
+  $('.change-view input').each(function(key,value){
+    $(value).prop('checked', false); 
+    $(value).parent().removeClass('checked');
+    if($(value).val() === className){
+      $(value).prop('checked', true);
+      $(value).parent().addClass('checked');
+    }
+  });
+  
+  $('.change-view').on('ifChanged', function (e) {
+    var t = $(e.target);
+
+    if (t.is(':checked')) {
+      var className = t.val();
+      setView(className);
+    }
+  });
+
+
+
+});
